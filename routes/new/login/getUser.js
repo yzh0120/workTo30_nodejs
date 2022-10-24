4fcc4579fcbcd731d72601d9cc7464e2dfcee495
yzh0120 <644193385@qq.com>
2022-10-24 11:53:24 +0800

    up

--- routes/new/login/getUser.js
+++ routes/new/login/getUser.js
@@ -7,9 +7,28 @@ module.exports = function (req, res) {
 				data: {
 					userName: item,
 					username: item,
+					roleArr: [item],
 					id: item,
 				}
 			})
 		}
 	}
 }
+/*
+module.exports = function (req, res) {
+	let arr = ['system','test']
+	for(let item of arr){
+		if(req.headers.authorization.includes(item) ){
+			res.send({
+				code: 200,
+				data:{
+					username: item,
+					roleArr: [item],
+					id:item,
+				}
+			})
+		}
+	}
+}
+
+*/
